8bd336dbfa8b2b56d52c4c0851a3b57cb787d3fc
NaiveInvestigator <72654596+NaiveInvestigator@users.noreply.github.com>
2026-05-28 01:31:17 +0600

    chore: fixed typo

--- init.lua
+++ init.lua
@@ -933,7 +933,7 @@ do
       -- See `:help blink-cmp-config-keymap` for defining your own keymap
       preset = 'default',
 
-      -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:chadwalchadwal
+      -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
     },
 
